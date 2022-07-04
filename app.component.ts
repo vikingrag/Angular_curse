2fd3bdee3e6b610376e0e48323e4dc874d0a67a9
vikingrag <100153634+vikingrag@users.noreply.github.com>
2022-07-04 10:27:01 +0200

    Добавлен комментарий к коду

--- app.component.ts
+++ app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 
+// экспортируем елементы
 export interface Card {
   title: string
   text: string
 }
-
+// декоратор Component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
+  // создаем переменную для работы с кнопкой, которая, при нажатии убирает карточки с проэкта и после очередного нажатия возвращает карточки обратно
   toggle = true
 
+  // создаем массив состоящий из обьектов с типами и названиями карточек в проэкте
   cards: Card[] = [
     { title: 'Card 1', text: 'This is card number 1' },
     { title: 'Card 2', text: 'This is card number 2' },
@@ -21,7 +25,7 @@ export class AppComponent {
 
   ]
 
-
+  // Прописываем функцию для кнопки, которая, при нажатии убирает карточки с проэкта и после очередного нажатия возвращает карточки обратно
   toggleCards() {
     this.toggle = !this.toggle
   }
--- card/card.component.ts
+++ card/card.component.ts
@@ -2,25 +2,34 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../app.component';
 
+// Прописываем куда необходимо вставить компоненты в проэкте, которые мы экспортируем
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.components.scss'],
 })
+
+// Прописываем експорт компонентов в ангуляр
 export class CardComponent implements OnInit {
 
+  // Импортируем с библиотеки Ангуляра значения для проэкта
   @Input() card: Card
   @Input() index: number
 
+  // Переменные с названиями
   title: string = 'Mi card Title'
   text: string = 'My sample text'
 
+  // Создаем переменную для указания даты в проэкте
   cardDate: Date = new Date()
 
+  // создаем переменную для цветов кнопок
   textColor = 'none'
 
+  // метод для обработки любых дополнительных задач инициализации.
   ngOnInit() { }
 
+  // Создаем функцию изменения текста при нажатии на кнопку в проэкте
   changeTitle() {
     this.card.title = 'TITLE HAS BEEN CHANGE!'
   }
